1b1c23b628ec07a816906635662c31105b3e6e3b
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-14 23:40:54 +0300

    Решение задачи 'Простое число' [#178590]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when5(){
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
+        boolean result = checkPrimeNumber.check(5);
+        Assert.assertThat(result, Is.is(true));
+    }
+
+    @Test
+    public void when19(){
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
+        boolean result = checkPrimeNumber.check(19);
+        Assert.assertThat(result, Is.is(true));
+    }
+
+    @Test
+    public void when4(){
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
+        boolean result = checkPrimeNumber.check(4);
+        Assert.assertThat(result, Is.is(false));
+    }
+
+    @Test
+    public void when120(){
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
+        boolean result = checkPrimeNumber.check(120);
+        Assert.assertThat(result, Is.is(false));
+    }
+}
