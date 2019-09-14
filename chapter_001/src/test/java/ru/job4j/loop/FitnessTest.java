9ea5616047852af477dceac07e788bafd70f07ba
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-14 23:17:21 +0300

    Решение задачи 'Протеиновая диета' [#178587]

---
+++ chapter_001/src/test/java/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik(){
+        Fitness fitness = new Fitness();
+        int expected = fitness.calc(95, 90);
+        Assert.assertThat(expected, Is.is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik(){
+        Fitness fitness = new Fitness();
+        int expected = fitness.calc(90, 95);
+        Assert.assertThat(expected, Is.is(1));
+    }
+
+    @Test
+    public void whenIvanLessByTwoNik(){
+        Fitness fitness = new Fitness();
+        int expected = fitness.calc(50, 90);
+        Assert.assertThat(expected, Is.is(2));
+    }
+}
