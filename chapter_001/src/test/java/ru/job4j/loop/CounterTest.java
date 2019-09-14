4fe4ca7a087c01d47119daf8233a3d30c036d0e3
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-14 22:24:03 +0300

    Решение задачи 'Подсчет суммы четных чисел в диапазоне' [#178582]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CounterTest {
+
+    @Test
+    public  void whenSummEvenNumbersFromOneToTenThenThirty(){
+        Counter counter = new Counter();
+        int expected = counter.add(0, 10);
+        Assert.assertThat(expected, Is.is(30));
+    }
+}
