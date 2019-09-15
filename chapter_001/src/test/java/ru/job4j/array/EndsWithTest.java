0dafa98e0ab1f1b3faee4464e5f911cd55219461
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-16 00:37:06 +0300

    Решение задачи 'Слово заканчивается с...' [#178604]

---
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenWordEndsWithPrefixThenTrue(){
+        char[] word = {'H','e','l','l','o'};
+        char[] pref = {'l','o'};
+        boolean result = EndsWith.endsWith(word, pref);
+        Assert.assertThat(result, Is.is(true));
+    }
+
+    @Test
+    public void whenWordNotEndsWithPrefixThenTrue(){
+        char[] word = {'H','e','l','l','o'};
+        char[] pref = {'l','a'};
+        boolean result = EndsWith.endsWith(word, pref);
+        Assert.assertThat(result, Is.is(false));
+    }
+}
