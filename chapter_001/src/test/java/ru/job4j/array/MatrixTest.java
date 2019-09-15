bddd248e76771b5bcfba8040f16bfa432f54e60d
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-16 02:56:51 +0300

    Решение задач 'Двумерный массив. Таблица умножения' [#178595]

---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MatrixTest {
+
+    @Test
+    public void when2On2(){
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(2);
+        int[][] expect = {
+                {1, 2},
+                {2, 4}
+        };
+        Assert.assertThat(table, Is.is(expect));
+    }
+}
