b41eea6ce28d971dfe639b437931b291b6784b76
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-15 19:01:33 +0300

    Правка задачи [#178591]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,15 +4,9 @@ public class PrimeNumber {
 
     public int calc(int finish){
         int count = 0;
+        CheckPrimeNumber checkPrime = new CheckPrimeNumber();
         for (int i = 2; i <= finish; i++){
-            boolean prime = true;
-            for (int j = 2; j < i; j++){
-                if (i % j == 0){
-                    prime = false;
-                    break;
-                }
-            }
-            if (prime)count++;
+            if (checkPrime.check(i))count++;
         }
         return count;
     }
