7ce622e322c139d5bd330b4666d62644f6269891
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-15 23:55:32 +0300

    Решение задачи 'Массив заполнен true или false' [#178597]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Check {
+
+    public boolean mono(boolean[] data){
+        boolean result = true;
+        boolean el = data[0];
+        for (int i = 0; i < data.length; i++){
+            if (data[i] != el){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckTest {
+
+    @Test
+    public void whenDataMonoTrueThenTrue(){
+        Check check = new Check();
+        boolean[] input = {true, true, true, true};
+        boolean rst = check.mono(input);
+        Assert.assertThat(rst, Is.is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoTrueThenTrue(){
+        Check check = new Check();
+        boolean[] input = {false, false, true, true};
+        boolean rst = check.mono(input);
+        Assert.assertThat(rst, Is.is(false));
+    }
+}
