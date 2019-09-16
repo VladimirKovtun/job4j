542ac27f79a42d48f32e5b7334e2dad43ad53378
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-16 04:40:52 +0300

    Решение задач 'Дeфрагментация массива' [#178600]

---
+++ chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DefragmentTest {
+
+    @Test
+    public void whenNotFirstNull(){
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] result = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertThat(result, Is.is(expected));
+    }
+
+    @Test
+    public void whenFirstNull(){
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] result = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertThat(result, Is.is(expected));
+    }
+}
