adbdf6233a17ba5a3224329d6b3065c624ef3d22
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-17 17:11:25 +0300

    Правка задачи [#178598]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -8,13 +8,13 @@ public class MatrixCheck {
             char sign = board[i][i];
             if (sign == 'X') {
                 int count = 0;
-                if (((i < 4) && board[i][i + 1] == 'X') || ((i > 1) && board[i][i - 1] == 'X')) {
+                if (((i < board.length - 1) && board[i][i + 1] == 'X') || ((i > 1) && board[i][i - 1] == 'X')) {
                     for (int k = 0; k < board.length; k++) {
                         if (board[i][k] == 'X') {
                             count++;
                         }
                     }
-                } else if (((i < 4) && board[i + 1][i] == 'X') || ((i > 1) && board[i - 1][i] == 'X')) {
+                } else if (((i < board.length - 1) && board[i + 1][i] == 'X') || ((i > 1) && board[i - 1][i] == 'X')) {
                     for (int k = 0; k < board.length; k++) {
                         if (board[k][i] == 'X') {
                             count++;
