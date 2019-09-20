11e563d4ff681fa59d5ed7b0297ff148f1e969e2
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-20 20:16:01 +0300

    Решение задачи 'Котенок гав и его котлета'[#178712]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        name = nick;
+    }
+
+    public void eat(String meat) {
+        food = meat;
+    }
+
+    public void show() {
+        System.out.printf("There are %s's %s %n", this.name, this.food);
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("chop");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
