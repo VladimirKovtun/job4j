8338cf8bfe3a4daa096c59fdedb7f8ad8785a2f8
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-23 22:06:36 +0300

    Правка задачи [#178732]

--- chapter_002/src/main/java/ru/job4j/pojo/Shop.java
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -3,25 +3,24 @@ package ru.job4j.pojo;
 
 public class Shop {
 
-    public Product[] delete(Product[] products, int index) {
+    public void delete(Product[] products, int index) {
         products[index] = null;
-        for (int i = index; i < products.length - 2; i++) {
+        for (int i = index; i < products.length - 1; i++) {
             if (products[i] == null && products[i + 1] != null) {
                 products[i] = products[i + 1];
                 products[i + 1] = null;
             }
         }
-        return products;
     }
 
     public static void main(String[] args) {
         Product[] products = {new Product("Milk", 10),
                               new Product("Bread", 4),
                               new Product("Egg", 19),
-                              null,
-                              null};
+                              new Product("Butter", 5),
+                              new Product("Sugar", 8)};
         Shop shop = new Shop();
-        Product[] delete = shop.delete(products, 1);
+        shop.delete(products, 3);
         for (int i = 0; i < products.length; i++) {
             if (products[i] != null) {
                 System.out.println(products[i].getName() + " " + products[i].getCount());
