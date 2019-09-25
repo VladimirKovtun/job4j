c67b076639de576e7e400f47c1a9ef7e265d61bb
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-25 23:59:34 +0300

    Правка задачи [#178707]

--- chapter_002/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/condition/Triangle.java
@@ -4,33 +4,35 @@ public class Triangle {
     private Point a;
     private Point b;
     private Point c;
+    private double ab;
+    private double bc;
+    private double ca;
 
     public Triangle(Point first, Point second, Point third) {
         this.a = first;
         this.b = second;
         this.c = third;
+        ab = a.distance(b);
+        bc = b.distance(c);
+        ca = c.distance(a);
     }
 
-    private boolean exist(Point a, Point b, Point c) {
-        double ab = a.distance(b);
-        double bc = b.distance(c);
-        double ca = c.distance(a);
+    private boolean exist() {
         return ab + bc > ca
                && ab + ca > bc
                && bc + ca > ab;
     }
 
-    private double period(Point a, Point b, Point c) {
-        return (a.distance(b) + b.distance(c) + c.distance(a)) / 2;
+    private double period() {
+        return (ab + bc + ca) / 2;
     }
 
     public double square() {
         double result = -1;
-        if (exist(this.a, this.b, this.c)) {
-            double period = period(this.a, this.b, this.c);
-            result = Math.sqrt(period * (period - this.a.distance(this.b))
-                               * period * (period - this.b.distance(this.c))
-                               * period * (period - this.c.distance(this.a)));
+        if (exist()) {
+            result = Math.sqrt(period() * (period() - ab)
+                               * period() * (period() - bc)
+                               * period() * (period() - ca));
         }
         return result;
     }
