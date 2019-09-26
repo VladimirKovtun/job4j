41000eda0a187cf39fbbeed0e46f37e142d3cb26
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-26 04:01:33 +0300

    Решение задачи. 'Тестирование. Подготовка данных.' [#182960]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,4 +16,26 @@ public class StartUITest {
         Item expected = new Item("Test");
         Assert.assertThat(created.getName(), Is.is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {"replaced item", item.getId()};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        Assert.assertThat(replaced.getName(), Is.is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item[] newItems = tracker.findByName("new item");
+        Assert.assertNull(newItems);
+    }
 }
