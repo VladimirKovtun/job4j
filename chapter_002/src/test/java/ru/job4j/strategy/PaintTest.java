2e1dbed354c9149539b2fa2f883d1d1df7eba683
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-28 01:03:49 +0300

    Решение задачи. 'Рефакторинг теста @Before @After'[#178689]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,20 +1,29 @@
 package ru.job4j.strategy;
 
 import org.hamcrest.core.Is;
-import org.hamcrest.core.StringContains;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class PaintTest {
+    private final PrintStream stdOut = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOut() {
+        System.setOut(new PrintStream(out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(stdOut);
+    }
 
     @Test
     public void whenDrawSquare() {
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         Assert.assertThat(new String(out.toByteArray()), Is.is(
                 new StringBuilder().append(" ______\n")
@@ -24,6 +33,18 @@ public class PaintTest {
                                    .append(System.lineSeparator())
                                    .toString())
         );
-        System.setOut(stdOut);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        Assert.assertThat(new String(out.toByteArray()), Is.is(
+                new StringBuilder().append("   /\\\n")
+                                   .append("  /  \\\n")
+                                   .append(" /    \\\n")
+                                   .append("/______\\\n")
+                                   .append(System.lineSeparator())
+                                   .toString())
+        );
     }
 }
