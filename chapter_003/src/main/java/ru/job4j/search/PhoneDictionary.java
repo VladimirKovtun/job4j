dc8792a6ebcefab2856b19f16545d3acc07b5dfe
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-10-05 00:11:20 +0300

    Решение задачи. 'Телефонный справочник на базе ArrayList'[#178663]

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,25 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public List<Person> find(String key) {
+        List<Person> findPerson  = new ArrayList<>();
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i).getName().contains(key) ||
+                persons.get(i).getSurname().contains(key) ||
+                persons.get(i).getAdress().contains(key) ||
+                persons.get(i).getPhone().contains(key)) {
+                findPerson.add(persons.get(i));
+            }
+        }
+        return findPerson;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.search;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phoneBook = new PhoneDictionary();
+        phoneBook.add(new Person("Vladimir", "Kovtun", "1234567", "Saint-Petersburg"));
+        List<Person> people = phoneBook.find("dim");
+        Assert.assertThat(people.iterator().next().getPhone(), Is.is("1234567"));
+    }
+
+    @Test
+    public void whenFindTwoPeopleByName() {
+        PhoneDictionary phoneBook = new PhoneDictionary();
+        phoneBook.add(new Person("Vladimir", "Kovtun", "1234567", "Saint-Petersburg"));
+        phoneBook.add(new Person("Nurlan", "Bay", "322", "Nur-Sultan"));
+        phoneBook.add(new Person("Vadim", "Petrov", "228", "Moscow"));
+        List<Person> people = phoneBook.find("dim");
+        Assert.assertThat(people.size(), Is.is(2));
+    }
+}
