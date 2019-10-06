27a87e3ccaf83f55a3f0d77a1a51bf540e26b78f
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-10-06 22:46:17 +0300

    Правка задачи [#34741]

--- chapter_001/src/main/java/ru/job4j/array/CoffeMachine.java
+++ chapter_001/src/main/java/ru/job4j/array/CoffeMachine.java
@@ -10,27 +10,31 @@ public class CoffeMachine {
         if (change == 0) {
             return new int[]{0};
         }
-        if (change < 0){
+        if (change < 0) {
             return new int[]{-1};
         }
-        int array = change % 5 == 0 ? change / 5 :
-                    change % 5 < 3 ? (change / 5) + 1 : (change / 5) + 2;
-        int[] oddMoney = new int[array];
+        int array = change % 10 == 0 ? change / 10 :
+                (change % 10) % 5 == 0 ? (change / 10) + 1 :
+                        (change / 5) < 3 ? (change / 10) + 2 : (change / 10) + 3;
+        int[] coins = new int[array];
         int index = 0;
         while (change > 0) {
-             if (change % 5 == 1 || change % 5 == 3){
-                oddMoney[index++] = 1;
+            if (change >= 10) {
+                coins[index++] = 10;
+                change -= 10;
+            } else if (change >= 5) {
+                coins[index++] = 5;
+                change -= 5;
+            } else if (change >= 2) {
+                coins[index++] = 2;
+                change -= 2;
+            } else {
+                coins[index++] = 1;
                 change -= 1;
-             } else if (change % 5 == 2 || change % 5 == 4) {
-                 oddMoney[index++] = 2;
-                 change -= 2;
-             } else {
-                 oddMoney[index++] = 5;
-                 change -= 5;
-             }
-         }
+            }
+        }
 
-        return oddMoney;
+        return coins;
     }
 }
 
--- chapter_001/src/test/java/ru/job4j/array/CoffeMachineTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CoffeMachineTest.java
@@ -8,36 +8,36 @@ public class CoffeMachineTest {
 
     @Test
     public void when60RubAndCoffee35ThenChangeIsAll5Rub() {
-        int[] result = CoffeMachine.changes(60, 35);
-        int[] expect = {5, 5, 5, 5, 5};
+        int[] result = CoffeMachine.changes(60, 55);
+        int[] expect = {5};
         Assert.assertThat(result, Is.is(expect));
     }
 
     @Test
     public void when60RubAndCoffee34ThenChangeIsOne1RubAndFive5Rub() {
-        int[] result = CoffeMachine.changes(60, 34);
-        int[] expect = {1, 5, 5, 5, 5, 5};
+        int[] result = CoffeMachine.changes(60, 35);
+        int[] expect = {10, 10, 5};
         Assert.assertThat(result, Is.is(expect));
     }
 
     @Test
     public void when60RubAndCoffee33ThenChangeIsOne2RubAndFive5Rub() {
-        int[] result = CoffeMachine.changes(60, 33);
-        int[] expect = {2, 5, 5, 5, 5, 5};
+        int[] result = CoffeMachine.changes(60, 30);
+        int[] expect = {10, 10, 10};
         Assert.assertThat(result, Is.is(expect));
     }
 
     @Test
     public void when60RubAndCoffee32ThenChangeIsOne1RubAndOne2RubAndFive5Rub() {
         int[] result = CoffeMachine.changes(60, 32);
-        int[] expect = {1, 2, 5, 5, 5, 5, 5};
+        int[] expect = {10, 10, 5, 2, 1};
         Assert.assertThat(result, Is.is(expect));
     }
 
     @Test
     public void when60RubAndCoffee31ThenChangeIsTwo2RubAndFive5Rub() {
         int[] result = CoffeMachine.changes(60, 31);
-        int[] expect = {2, 2, 5, 5, 5, 5, 5};
+        int[] expect = {10, 10, 5, 2, 2};
         Assert.assertThat(result, Is.is(expect));
     }
 
