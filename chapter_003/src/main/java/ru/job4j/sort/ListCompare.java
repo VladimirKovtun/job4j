653495fb373187cc2a854b35486188757add45c2
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-10-08 02:59:54 +0300

    Решение задачи 'Компаратор для строк.'[#178654]

---
+++ chapter_003/src/main/java/ru/job4j/sort/ListCompare.java
@@ -0,0 +1,24 @@
+package ru.job4j.sort;
+
+import java.util.Comparator;
+
+public class ListCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        int result = 0;
+        int count = 0;
+        int length = o1.length() < o2.length() ? o1.length() : o2.length();
+        for (int i = 0; i < length; i++) {
+            if (o1.charAt(i) < o2.charAt(i) || (count == (length - 1) && o1.length() != o2.length())) {
+                result = -1;
+                break;
+            } else if (o1.charAt(i) > o2.charAt(i)) {
+                result = 1;
+                break;
+            }
+            count++;
+        }
+        return result;
+    }
+}
