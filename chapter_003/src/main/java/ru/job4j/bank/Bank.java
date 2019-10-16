5b545bc358260600395736d83538072561c15ff6
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-10-17 02:23:24 +0300

    Решение задачи 'Тестовое задание из модуля коллекции Lite переделать на Stream API.'[#178544]

--- chapter_003/src/main/java/ru/job4j/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/bank/Bank.java
@@ -24,18 +24,30 @@ public class Bank {
     }
 
     public void addAccountToUser(String passport, Account account) {
-        for (User next : dataBase.keySet()) {
+        dataBase.keySet()
+                .stream()
+                .filter(key -> key.getPassport().equals(passport)
+                        && account != null && !dataBase.get(key).contains(account))
+                .findAny()
+                .ifPresent(obj -> dataBase.get(obj).add(account));
+        /*for (User next : dataBase.keySet()) {
             if (next.getPassport().equals(passport)) {
                 if (account != null && !dataBase.get(next).contains(account)) {
                     dataBase.get(next).add(account);
                     break;
                 }
             }
-        }
+        }*/
     }
 
     public void deleteAccountFromUser(String passport, Account account) {
-        for (User next : dataBase.keySet()) {
+        dataBase.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(passport)
+                        && dataBase.get(user).contains(account))
+                .findFirst()
+                .ifPresent(user -> dataBase.get(user).remove(account));
+        /*for (User next : dataBase.keySet()) {
             if (next.getPassport().equals(passport)) {
                 for (Account acc : dataBase.get(next)) {
                     if (acc.equals(account)) {
@@ -44,22 +56,33 @@ public class Bank {
                     }
                 }
             }
-        }
+        }*/
     }
 
     public List<Account> getUserAccounts(String passport) {
-        List<Account> list = new ArrayList<>();
+        return dataBase.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().getPassport().equals(passport))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElse(null);
+
+        /*List<Account> list = new ArrayList<>();
         for (User next : dataBase.keySet()) {
             if (next.getPassport().equals(passport)) {
                 list = dataBase.get(next);
                 break;
             }
         }
-        return list;
+        return list;*/
     }
 
     public Account getAcc(String passport, String requisite) {
-        Account account = null;
+        return getUserAccounts(passport).stream()
+                .filter(account -> account.getRequisites().equals(requisite))
+                .findFirst()
+                .orElse(null);
+        /*Account account = null;
         List<Account> userAccounts = getUserAccounts(passport);
         for (Account acc : userAccounts) {
             if (acc.getRequisites().equals(requisite)) {
@@ -68,7 +91,7 @@ public class Bank {
                 break;
             }
         }
-        return account;
+        return account;*/
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
