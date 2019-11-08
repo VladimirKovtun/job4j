31d43b7e89547000c7daab359d23339e9a89e2ad
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-11-08 17:25:40 +0300

    first try [#191705]

--- chapter_006/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_006/src/main/java/ru/job4j/io/Analizy.java
@@ -7,30 +7,22 @@ public class Analizy {
 
     public void unAvailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+             BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             String line;
-            String[] str;
-            StringBuilder resultString;
+            StringBuilder resultString = new StringBuilder();
+            boolean sw = true;
             while (reader.ready()) {
                 line = reader.readLine();
                 if (!line.trim().isEmpty()) {
-                    str = line.split(" ");
-                    int type = Integer.parseInt(str[0]);
-                    if (type == 400 || type == 500) {
-                        resultString = new StringBuilder(str[1]);
-                        while (reader.ready()) {
-                            line = reader.readLine();
-                            if (!line.trim().isEmpty()) {
-                                str = line.split(" ");
-                                type = Integer.parseInt(str[0]);
-                                if (type == 200 || type == 300) {
-                                    resultString.append(";").append(str[1]).append(";").append("\n");
-                                    writer.write(resultString.toString());
-                                    writer.flush();
-                                    break;
-                                }
-                            }
-                        }
+                    if ((line.startsWith("400") || line.startsWith("500")) && sw) {
+                        resultString.append(line.split(" ")[1]);
+                        sw = false;
+                    } else if ((line.startsWith("200") || line.startsWith("300")) && !sw) {
+                        resultString.append(";").append(line.split(" ")[1]).append(";").append("\n");
+                        writer.write(resultString.toString());
+                        writer.flush();
+                        resultString = new StringBuilder();
+                        sw = true;
                     }
                 }
             }
