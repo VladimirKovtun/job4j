f677f35cc24de3aadc969715b8c27ff3f57c9204
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-11-10 16:28:30 +0300

    add method write. [#191705]

--- chapter_006/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_006/src/main/java/ru/job4j/io/Analizy.java
@@ -1,13 +1,14 @@
 package ru.job4j.io;
 
-
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizy {
 
     public void unAvailable(String source, String target) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(source));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            List<String> logLine = new ArrayList<>();
             String line;
             StringBuilder resultString = new StringBuilder();
             boolean sw = true;
@@ -18,13 +19,24 @@ public class Analizy {
                         resultString.append(line.split(" ")[1]);
                         sw = false;
                     } else if ((line.startsWith("200") || line.startsWith("300")) && !sw) {
-                        resultString.append(";").append(line.split(" ")[1]).append(";").append("\n");
-                        writer.write(resultString.toString());
-                        writer.flush();
+                        resultString.append(";").append(line.split(" ")[1]).append(";");
+                        logLine.add(resultString.toString());
                         resultString = new StringBuilder();
                         sw = true;
                     }
                 }
+            }
+            writeOn(logLine, target);
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    private void writeOn(List<String> logList, String target) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            for (String line : logList) {
+                writer.write(line + "\n");
+                writer.flush();
             }
         } catch (IOException exc) {
             exc.printStackTrace();
