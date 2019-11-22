f0eaecc07e717190ee3c82b252b1dfca38ce6cf0
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-11-22 15:10:21 +0300

    test Predicate#1 [#191703]

--- chapter_006/src/main/java/ru/job4j/io/finder/FindUtil.java
+++ chapter_006/src/main/java/ru/job4j/io/finder/FindUtil.java
@@ -21,59 +21,30 @@ public class FindUtil {
         if (args.validateKey()) {
             List<File> files = new Search().allFiles(args.getFileNameDir());
             resList = seekByPredicate(files, getPredicat());
-            //resList = seekByCriteria(files, args.getFlag(), args.getCriteria());
             write(resList, args.getFileNameSave(), args.getFileNameDir());
         }
     }
 
-    private BiPredicate<File, Args> getPredicat() {
+    private Predicate<File> getPredicat() {
         if ("-f".equals(args.getFlag())) {
-            return (x, y) -> x.getName().equals(y.getCriteria());
+            return x -> x.getName().equals(args.getCriteria());
         } else if ("-m".equals(args.getFlag())) {
-            return (x, y) -> x.getName().endsWith(y.getCriteria().substring(1));
+            return x -> x.getName().endsWith(args.getCriteria().substring(1));
         } else if ("-r".equals(args.getFlag())) {
-            return (x, y) -> Pattern.compile(y.getCriteria()).matcher(x.getName()).find();
+            return x -> Pattern.compile(args.getCriteria()).matcher(x.getName()).find();
         }
         return null;
     }
 
-    private List<File> seekByPredicate(List<File> list, BiPredicate<File, Args> predicate) {
+    private List<File> seekByPredicate(List<File> list, Predicate<File> predicate) {
         List<File> resList = new ArrayList<>();
         for (File file : list) {
-            if (predicate.test(file, args)) {
+            if (predicate.test(file)) {
                 resList.add(file);
             }
         }
         return resList;
     }
-
-    /*private List<File> seekByCriteria(List<File> list, String flag, String criteria) {
-        List<File> resList = new ArrayList<>();
-        if (list.size() != 0) {
-            if ("-f".equals(flag)) {
-                for (File file : list) {
-                    if (file.getName().equals(criteria)) {
-                        resList.add(file);
-                        break;
-                    }
-                }
-            } else if ("-m".equals(flag)) {
-                for (File file : list) {
-                    String fileName = file.getName();
-                    String criteriaExt = criteria.substring(1);
-                    if (fileName.endsWith(criteriaExt)) {
-                        resList.add(file);
-                    }
-                }
-            } else if ("-r".equals(flag)) {
-                Pattern pattern = Pattern.compile(criteria);
-                list.stream()
-                        .filter(x -> pattern.matcher(x.getName()).find())
-                        .forEach(resList::add);
-            }
-        }
-        return resList;
-    }*/
 
     private void write(List<File> resList, String fileName, String directory) {
         if (resList.size() != 0) {
