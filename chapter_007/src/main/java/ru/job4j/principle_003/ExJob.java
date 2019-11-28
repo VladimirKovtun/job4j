551df04a20bf547d4fbbf2113c7d0f5f5cf79828
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-11-28 15:40:14 +0300

    log4j2

---
+++ chapter_007/src/main/java/ru/job4j/principle_003/ExJob.java
@@ -0,0 +1,17 @@
+package ru.job4j.principle_003;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ExJob {
+    private final static Logger LOG = LogManager.getLogger(ExJob.class);
+
+    public static void main(String[] args) {
+        int version = 1;
+        LOG.trace("trace message {}", version);
+        LOG.debug("debug message {}", version);
+        LOG.info("info message {}", version);
+        LOG.warn("warn message {}", version);
+        LOG.error("error message {}", version);
+    }
+}
